c5a00049e3c500856a8f0e73e185948b6f5d5dbf
dylanbhe <dylanherzog@gmail.com>
2023-04-24 20:55:45 +0100

    test commit message

--- src/MaxAndMin.java
+++ src/MaxAndMin.java
@@ -3,20 +3,15 @@ import java.util.Random; //import Random class
 public class MaxAndMin {
     public static void main(String[] args) {
         Random rand = new Random(); //initiate Random instance called rand
-        //int funNum = rand.nextInt(10) + 1; //bound is 1 to 10 (without +1, 0 - 10)
-
-
-        //create and initialise integer array and pre-fill with random values. (length = 10)
         int [] intArray = new int [10]; //initialise empty list with length of 10
 
-
         System.out.println("This is the list of numbers: ");
-
         for (int index = 0; index < intArray.length; index ++ ) { //loop over int.Array to create array
             int funNum = rand.nextInt(10) + 1; // funNum are random numbers. boundary is 1 to 10 (without +1, 0 - 10)
             intArray[index] = funNum; //populate list with random numbers
             System.out.print(intArray[index] + " "); //print out each number followed by a space
         }
+
         int maxNum = intArray[0]; //declare and initialise maxNum
         int minNum = intArray[0];
 
